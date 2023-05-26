cc020daed57f0e204557ec7afc16e647a533319e
Aleksandar Petrov <sasho1320@gmail.com>
2023-05-26 17:30:25 +0300

    Update settings.py file

--- HutSeeker/HutSeeker/settings.py
+++ HutSeeker/HutSeeker/settings.py
@@ -10,11 +10,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -68,17 +63,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'HutSeeker.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': {
