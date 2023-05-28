28760019d8536ecf69918d12443070ad3ea4ba06
Aleksandar Petrov <sasho1320@gmail.com>
2023-05-28 20:53:19 +0300

    Update urls

--- HutSeeker/HutSeeker/urls.py
+++ HutSeeker/HutSeeker/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from huts import views
+from .huts import views
 
 router = routers.DefaultRouter()
 
