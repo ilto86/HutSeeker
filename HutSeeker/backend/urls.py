927bfae93232a3ac7764c222d3306af387470a03
Ilter Zekerie <103379086+ilto86@users.noreply.github.com>
2023-06-27 17:34:02 +0200

    Added new URL patterns for Huts model
    Defined new URL patterns to enable access and interaction with Huts endpoints.

--- HutSeeker/backend/urls.py
+++ HutSeeker/backend/urls.py
@@ -8,6 +8,9 @@ from backend.huts import views
 router = routers.DefaultRouter()
 router.register(r'huts', views.HutsView, 'huts')
 router.register(r'approach', views.ApproachView, 'approach')
+router.register(r'month', views.MonthView, 'month')
+router.register(r'services', views.ServicesView, 'services')
+router.register(r'weather_condition', views.WeatherConditionView, 'weather_condition')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
